ddb1a7842c40ed440c03349c04420afe4ea20c3d
oogorodnyk <oogorodnyk@atlassian.com>
2020-03-22 19:33:15 +0200

    add markup and state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import './App.scss';
+import { AppState, changeNewUserName, addUser, selectUser, changeNewHobby, PassionLevel, addHobby } from '.';
 
 function App() {
+  const users = useSelector((state: AppState) => state.users);
+  const hobbies = useSelector((state: AppState) => state.hobbies);
+  const newUserName = useSelector((state: AppState) => state.newUserName);
+  const newHobby = useSelector((state: AppState) => state.newHobby);
+  const selectedUser = useSelector((state: AppState) => state.selectedUser);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      User hobbies
+      <div className='table'>
+        <div className='user-list'>
+          <div className='user-form'>
+            <input
+              placeholder='Enter user name'
+              className='user-name'
+              value={newUserName}
+              onChange={e => dispatch(changeNewUserName(e.currentTarget.value))}
+            />
+            <button className='add-user' onClick={() => dispatch(addUser(newUserName, users.length + 1))}>Add</button>
+          </div>
+          {users.map(user => (
+            <div
+              onClick={() => dispatch(selectUser(user.id))}
+              key={user.id}
+              className={selectedUser === user.id ? 'selected-user' : ''}
+            >
+              User: {user.name}
+            </div>
+          ))}
+        </div>
+        <div className='hobbies-list'>
+          <div className='hobby-form'>
+            <select
+              onChange={e => dispatch(changeNewHobby(
+                newHobby?.description || '',
+                e.currentTarget.value as PassionLevel || 'Low',
+                newHobby?.since || ''
+              ))}
+              value={newHobby?.passionLevel || 'Low'}
+              placeholder='Select passion level'
+              className='passion-level'
+            >
+              <option value='Low'>Low</option>
+              <option value='Medium'>Medium</option>
+              <option value='High'>High</option>
+              <option value='Very-High'>Very-High</option>
+            </select>
+            <input
+              onChange={e => dispatch(changeNewHobby(
+                e.currentTarget.value || '',
+                newHobby?.passionLevel || 'Low',
+                newHobby?.since || ''
+              ))}
+              value={newHobby?.description || ''}
+              placeholder='Enter user hobby'
+              className='user-hobby'
+            />
+            <input
+              onChange={e => dispatch(changeNewHobby(
+                newHobby?.description || '',
+                newHobby?.passionLevel || 'Low',
+                e.currentTarget.value || ''
+              ))}
+              value={newHobby?.since || ''}
+              placeholder='Enter year'
+              className='since-year'
+            />
+            <button
+              className='add-hobby'
+              disabled={!selectedUser}
+              onClick={() => selectedUser && dispatch(addHobby(
+                selectedUser,
+                newHobby?.description || '',
+                newHobby?.passionLevel || 'Low',
+                newHobby?.since || ''
+              ))}
+            >
+              Add
+            </button>
+          </div>
+          {hobbies.filter(hobby => hobby.userId === selectedUser).map(hobby =>
+            <div key={hobby.description+hobby.passionLevel+hobby.since}>{hobby.description}</div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,126 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export type PassionLevel = 'Low' | 'Medium' | 'High' | 'Very-High';
+
+type Hobby = {
+  userId: number;
+  passionLevel: PassionLevel;
+  description: string;
+  since: string;
+}
+
+type User = {
+  id: number;
+  name: string;
+}
+
+export type AppState = {
+  users: User[];
+  hobbies: Hobby[];
+  newUserName: string;
+  newHobby?: Hobby;
+  selectedUser?: number;
+}
+
+const ADD_USER = 'ADD_USER';
+interface AddUserAction {
+  type: typeof ADD_USER
+  payload: User
+}
+export const addUser = (name: string, id: number): AddUserAction => {
+  return {
+     type: ADD_USER,
+     payload: { name, id }
+   }
+}
+
+const ADD_HOBBY = 'ADD_HOBBY';
+interface AddHobbyAction {
+  type: typeof ADD_HOBBY
+  payload: Hobby
+}
+export const addHobby = (userId: number, description: string, passionLevel: PassionLevel, since: string): AddHobbyAction => {
+  return {
+     type: ADD_HOBBY,
+     payload: { userId, description, passionLevel, since }
+   }
+}
+
+const CHANGE_NEW_USER_NAME = 'CHANGE_NEW_USER_NAME';
+interface ChangeNewUserNameAction {
+  type: typeof CHANGE_NEW_USER_NAME
+  payload: string
+}
+export const changeNewUserName = (name: string): ChangeNewUserNameAction => {
+  return {
+     type: CHANGE_NEW_USER_NAME,
+     payload: name
+   }
+}
+
+const CHANGE_NEW_HOBBY = 'CHANGE_NEW_HOBBY';
+interface ChangeNewHobbyAction {
+  type: typeof CHANGE_NEW_HOBBY
+  payload: Hobby
+}
+export const changeNewHobby = (description: string, passionLevel: PassionLevel, since: string): ChangeNewHobbyAction => {
+  return {
+     type: CHANGE_NEW_HOBBY,
+     payload: { userId: 0, description, passionLevel, since }
+   }
+}
+
+const SELECT_USER = 'SELECT_USER';
+interface SelectUserAction {
+  type: typeof SELECT_USER
+  payload: number
+}
+export const selectUser = (id: number): SelectUserAction => {
+  return {
+     type: SELECT_USER,
+     payload: id
+   }
+}
+
+type AppActions = AddUserAction | AddHobbyAction | ChangeNewUserNameAction | ChangeNewHobbyAction | SelectUserAction;
+
+const reducer = (state: AppState = initialState, action: AppActions): AppState => {
+  switch (action.type) {
+    case ADD_USER:
+      return {...state, users: [...state.users, action.payload], newUserName: ''}
+    case ADD_HOBBY:
+      return {...state, hobbies: [...state.hobbies, action.payload], newHobby: undefined}
+    case CHANGE_NEW_USER_NAME:
+      return {...state, newUserName: action.payload}
+    case CHANGE_NEW_HOBBY:
+      return {...state, newHobby: action.payload}
+    case SELECT_USER:
+      return {...state, selectedUser: action.payload, newHobby: undefined}
+    default:
+      return state;
+  }
+}
+
+const initialState: AppState = {
+  users: [],
+  hobbies: [],
+  newUserName: ''
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
